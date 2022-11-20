b52fc5f4d6b61613d0ed07e09d537997d9a1925c
I553707 <stefan.anca@sap.com>
2022-11-20 20:21:33 +0200

    add application

--- src/main/java/com/pies/kvstore/Main.java
+++ src/main/java/com/pies/kvstore/Main.java
@@ -1,7 +1,17 @@
 package com.pies.kvstore;
 
+import jetbrains.exodus.env.Environment;
+import jetbrains.exodus.env.Environments;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        try (Environment env = Environments.newInstance("tmp/storage")) {
+            var app = new KVStoreApp(env);
+            var server = new GrpcServer(app, 26658);
+            server.start();
+            server.blockUntilShutdown();
+        }
     }
 }
